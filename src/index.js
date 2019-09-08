11fe2af0cb18e35976c4f51cd60867fb639bec89
Aideen Fay <me@aideen.dev>
2019-09-08 22:35:01 +0100

    fix: merge issue

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import { render } from "react-dom";
-<<<<<<< HEAD
 import App from "./components/App";
-=======
-import App from "./components/AppPage";
->>>>>>> f3b971e72018c4de7c685b618ecad4c3367bab8e
 
 render(<App />, document.getElementById("root"));
